7d41644e475b3d686574b9aeb92cfb82a8d4fb8a
Harish-Elangovan-10 <harishelango2005@gmail.com>
2025-03-02 08:26:21 +0530

    Added loading animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
+    import LoadingAnimation from '$lib/loadingAnimation.svelte';
+    import { isPageLoading } from '$lib/pageLoading';
 	import '../app.css';
 	let { children } = $props();
 </script>
+
+{#if $isPageLoading}
+    <div class="fixed inset-0 flex items-center justify-center w-full h-screen bg-black/75 backdrop-blur-sm z-50">
+        <LoadingAnimation />
+    </div>
+{/if}
 
 {@render children()}
