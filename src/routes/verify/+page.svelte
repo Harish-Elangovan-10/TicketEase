e3eb4321f56a46d03b79fd54e6ad72bbd13a3b9d
Harish-Elangovan-10 <harishelango2005@gmail.com>
2025-03-17 12:06:35 +0530

    OTP verification UI created

--- src/routes/verify/+page.svelte
+++ src/routes/verify/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+    import { sendOTP, verifyOTP } from "$lib";
+    import { user } from "$lib/auth";
+    import toast, { Toaster } from "svelte-french-toast";
+    import { onMount } from "svelte";
+    import { startLoading, stopLoading } from "$lib/pageLoading";
+
     let otpValues: string[] = Array(6).fill('');
     let inputRefs: HTMLInputElement[] = [];
-    let error: string = '';
+    $:email = $user?.email ?? '';
+    $:name = $user?.firstName ?? 'User';
 
     const handleInput = (index: number, event: Event) => {
         const input = event.target as HTMLInputElement;
@@ -53,27 +60,64 @@
         }
     };
 
-    const handleVerify = () => {
-        const otp = otpValues.join('');
-        if(otp.length !== 6) {
-            error = 'Please enter all 6 digits';
-            return;
+    const handleSendOTP = async () => {
+        try {
+            startLoading();
+            await sendOTP(email, name);
+            console.log("OTP sent successfully!");
+            stopLoading();
+        } catch (err) {
+            stopLoading();
+            console.error("Failed to send OTP: ", err);
         }
     };
+
+    const handleVerifyOTP = async () => {
+        const otp = otpValues.join('');
+        if(otp.length !== 6) {
+            toast.error('Please enter all 6 digits', {
+                duration: 5000,
+                style: 'border-radius: 10px; background: #222; color: #fff; padding-left: 15px; border: 2px solid #333; margin-top: 20px;',
+            });
+            return;
+        }
+        try {
+            startLoading();
+            const response = await verifyOTP(email, otp);
+            if(response.success) {
+                toast.success(response.message, {
+                    duration: 5000,
+                    style: 'border-radius: 10px; background: #222; color: #fff; padding-left: 15px; border: 2px solid #333; margin-top: 20px;',
+                });
+                stopLoading();
+            } else {
+                stopLoading();
+                throw new Error("Invalid OTP");
+            }
+        } catch (err) {
+            stopLoading();
+            toast.error("Invalid OTP. Please try again", {
+                duration: 5000,
+                style: 'border-radius: 10px; background: #222; color: #fff; padding-left: 15px; border: 2px solid #333; margin-top: 20px;',
+            });
+        }
+    };
+
+    onMount(() => {
+        stopLoading();
+        // handleSendOTP();
+    });
 </script>
 
 <div class="min-h-screen bg-gradient-to-br from-gray-900 to-black text-gray-400 flex items-center justify-center">
+    <Toaster />
     <div class="flex flex-col items-center space-y-5">
         <h1 class="text-4xl font-bold bg-gradient-to-r from-lime-500 to-emerald-500 bg-clip-text text-transparent">
             Enter Verification Code
         </h1>
-        {#if error}
-			<p class="bg-gradient-to-r from-red-500 to-red-700 bg-clip-text text-transparent pb-3">{error}</p>
-		{:else}
-			<h2 class="text-gray-400 pb-3">
-				We've sent a code to your email address
-			</h2>
-		{/if}
+		<h2 class="text-gray-400 pb-3">
+			We've sent a code to your email address
+		</h2>
 
         <div class="w-md bg-gradient-to-br from-gray-900 to-black rounded-xl p-8 border-[2px] border-gray-800 backdrop-blur-sm">
             <div class="flex justify-center gap-2.5 mb-8">
@@ -99,9 +143,20 @@
             <button 
                 class="w-full py-3 px-4 rounded-xl bg-gradient-to-br from-lime-500 to-emerald-500 hover:from-lime-600 hover:to-emerald-600
                 text-black font-medium focus:outline-none focus:from-teal-500 focus:to-green-500 transition-all duration-200"
+                onclick={handleVerifyOTP}
             >
                 Verify Code
             </button>
+
+            <p class="place-self-center mt-6">
+                Didn't receive your code? <button 
+                    class="bg-gradient-to-r from-lime-400 to-emerald-400 hover:from-lime-600 hover:to-emerald-600 
+                    bg-clip-text text-transparent"
+                    onclick={handleSendOTP}
+                >
+                    Resend
+                </button>
+            </p>
         </div>
     </div>
 </div>
